1cc0c07570e05985ff34ff59d932715b4392ba73
Evgeniy Opryshko <uncesses@gmail.com>
2019-01-01 12:48:56 +0200

    Update logic login contoller

--- SaleManager/Controller/Main Controller/Main.swift
+++ SaleManager/Controller/Main Controller/Main.swift
@@ -35,15 +35,19 @@ class Main: UIViewController {
         let login = createNavController(controller: Controller.init(storyboard: "AuthStoryboard", identifier: "LoginViewController", image: "profile", selectedImage: "profileSelected"))
         
         Auth.auth().addStateDidChangeListener() { auth, user in
-            if user != nil {
-                let controllerArray = [product, manager]
-                self.tabBarCnt.viewControllers = controllerArray.map{ UINavigationController.init(rootViewController: $0)}
-                print("controllerArray = [product, manager]")
+    
+            if let user = user {
+                if user.email == "user@i.ua" {
+                    let controllerArray = [product, manager]
+                    self.tabBarCnt.viewControllers = controllerArray.map{ UINavigationController.init(rootViewController: $0)}
+                    print("controllerArray = [product, manager]")
+                }
             } else {
                 let controllerArray = [product, login]
                 print("controllerArray = [product, login]")
                 self.tabBarCnt.viewControllers = controllerArray.map{ UINavigationController.init(rootViewController: $0)}
             }
+            
             
         }
         
