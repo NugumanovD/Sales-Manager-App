f9a8adcf30745a75ac48982f39a65bed0c0d4bca
Evgeniy Opryshko <uncesses@gmail.com>
2019-01-03 22:46:01 +0200

    commit

--- SaleManager/Controller/Sales List/SalesListController.swift
+++ SaleManager/Controller/Sales List/SalesListController.swift
@@ -73,6 +73,7 @@ extension SalesListController: UITableViewDataSource, UITableViewDelegate {
             print("В архив")
         }
         
+        
         delete.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         archive.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         return [delete,archive]
